f5953c18ed0b6b5ee0786f591dd79208de9609f0
Martin Tudor <martcan27@gmail.com>
2020-09-05 19:17:29 +0100

    update useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ const matrixContainer: React.CSSProperties = {
 
 type AppState = {
     matrix: number[][];
+    ticking: boolean;
+    speed: number;
 };
 
 type Action =
@@ -30,7 +32,11 @@ type Action =
           type: "TOGGLE_PAINT";
           payload: [number, number];
       }
-    | { type: "CLICK"; payload: number[][] };
+    | { type: "CLICK"; payload: number[][] }
+    | {
+          type: "IS_TICKING";
+          payload: boolean;
+      };
 
 const appReducer = (state: AppState, action: Action): AppState => {
     switch (action.type) {
@@ -39,6 +45,9 @@ const appReducer = (state: AppState, action: Action): AppState => {
             const matrix = state.matrix.map((arr) => arr.slice());
             matrix[row][col] = matrix[row][col] === 0 ? 1 : 0;
             return { ...state, matrix };
+        }
+        case "IS_TICKING": {
+            return { ...state, ticking: !state.ticking };
         }
         case "CLICK": {
             return { ...state, matrix: action.payload };
@@ -49,7 +58,27 @@ const appReducer = (state: AppState, action: Action): AppState => {
 };
 
 function App() {
-    const [state, dispatch] = React.useReducer(appReducer, { matrix });
+    const [state, dispatch] = React.useReducer(appReducer, {
+        matrix,
+        ticking: false,
+        speed: 200,
+    });
+    const timerID: { current: number | undefined } = React.useRef();
+
+    React.useEffect(() => {
+        console.log("use effect");
+        if (!state.ticking) return;
+        timerID.current = window.setTimeout(
+            () =>
+                dispatch({
+                    type: "CLICK",
+                    payload: lifeOrDeath(state.matrix),
+                }),
+            state.speed
+        );
+
+        return () => window.clearTimeout(timerID.current);
+    }, [state.matrix, state.ticking, state.speed]);
 
     return (
         <div style={container}>
@@ -72,11 +101,16 @@ function App() {
                 <button
                     onClick={() =>
                         dispatch({
-                            type: "CLICK",
-                            payload: lifeOrDeath(state.matrix),
+                            type: "IS_TICKING",
+                            payload: !state.ticking,
                         })
                     }
                     className="myButton"
+                    style={
+                        state.ticking
+                            ? { background: "red" }
+                            : { background: "green" }
+                    }
                 >
                     Tick
                 </button>
