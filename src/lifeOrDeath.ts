a7dd76c9ef8e6a0b9e665881ad4c36fec4bdcd9d
MagicMart <martcan27@gmail.com>
2021-12-05 22:06:59 +0000

    refactor

--- src/lifeOrDeath.ts
+++ src/lifeOrDeath.ts
@@ -23,8 +23,7 @@ export function lifeOrDeath(dataMatrix: Matrix): Matrix {
     as determined by the rules */
     return dataMatrix.map((row, i): (0 | 1)[] => {
         return row.map((current, j): 0 | 1 => {
-
-            const sum = sumSurroundingCells(dataMatrix, [i, j], size)
+            const sum = sumSurroundingCells(dataMatrix, [i, j], size);
 
             // apply the rules of the Conway's game of life.
             // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
@@ -41,22 +40,84 @@ export function lifeOrDeath(dataMatrix: Matrix): Matrix {
 
 /** sum is the sum of values in the surrounding cells
  * (in clockwise order)
-*/
-export function sumSurroundingCells(dataMatrix: Matrix, [i, j]: [number, number], size: number) {
-    return dataMatrix[mayBeTransport(i - 1, size)][j] +
-        dataMatrix[mayBeTransport(i - 1, size)][
-        mayBeTransport(j + 1, size)
-        ] +
-        dataMatrix[i][mayBeTransport(j + 1, size)] +
-        dataMatrix[mayBeTransport(i + 1, size)][
-        mayBeTransport(j + 1, size)
-        ] +
-        dataMatrix[mayBeTransport(i + 1, size)][j] +
-        dataMatrix[mayBeTransport(i + 1, size)][
-        mayBeTransport(j - 1, size)
-        ] +
-        dataMatrix[i][mayBeTransport(j - 1, size)] +
-        dataMatrix[mayBeTransport(i - 1, size)][
-        mayBeTransport(j - 1, size)
-        ];
+ */
+export function sumSurroundingCells(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+) {
+    return (
+        getTop(dataMatrix, [i, j], size) +
+        getTopRight(dataMatrix, [i, j], size) +
+        getRight(dataMatrix, [i, j], size) +
+        getBottomRight(dataMatrix, [i, j], size) +
+        getBottom(dataMatrix, [i, j], size) +
+        getBottomLeft(dataMatrix, [i, j], size) +
+        getLeft(dataMatrix, [i, j], size) +
+        getTopLeft(dataMatrix, [i, j], size)
+    );
+}
+
+export function getTop(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[mayBeTransport(i - 1, size)][j];
+}
+
+export function getTopRight(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[mayBeTransport(i - 1, size)][mayBeTransport(j + 1, size)];
+}
+
+export function getRight(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[i][mayBeTransport(j + 1, size)];
+}
+
+export function getBottomRight(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[mayBeTransport(i + 1, size)][mayBeTransport(j + 1, size)];
+}
+
+export function getBottom(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[mayBeTransport(i + 1, size)][j];
+}
+
+export function getBottomLeft(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[mayBeTransport(i + 1, size)][mayBeTransport(j - 1, size)];
+}
+
+export function getLeft(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[i][mayBeTransport(j - 1, size)];
+}
+
+export function getTopLeft(
+    dataMatrix: Matrix,
+    [i, j]: [number, number],
+    size: number
+): number {
+    return dataMatrix[mayBeTransport(i - 1, size)][mayBeTransport(j - 1, size)];
 }
