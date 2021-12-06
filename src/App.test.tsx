168901bbdbf1dd54e9e53f9eb5311639ac2307cc
Martin Tudor <martcan27@gmail.com>
2021-12-06 15:34:18 +0000

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-    test("renders App component", () => {
+    it("renders App component", () => {
         const result = render(<App />);
         expect(result).toBeTruthy();
+    });
+    it("renders the correct title", () => {
+        render(<App />);
+        expect(screen.getByText("Game of Life")).toBeInTheDocument();
     });
 });
